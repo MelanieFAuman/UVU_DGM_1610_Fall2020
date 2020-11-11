7b6f46eccc9822a5fec8d83578548e122645fabe
Melanie <10803592@my.uvu.edu>
2020-11-11 16:06:47 -0700

    code outline

---
+++ Simpler Personal Project - Melanie Auman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    //Basic movement
+    public float speed = 10f;
+
+    // Jumping Ablity
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    //Game Over and death
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        //Jumping
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+        //Boundries
+      
+    }
+
+    //Detect ground and game over
+    private void OnCollisionEnter(Collision collision)
+    {
+        //End game action
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over! The Wolf has Feed!");
+        }
+    }
+}
