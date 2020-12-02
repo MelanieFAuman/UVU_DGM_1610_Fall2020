48011dc638cbbe9fd500c773ed8c021c86aff3e6
Melanie <10803592@my.uvu.edu>
2020-12-02 15:18:36 -0700

    Basic codes for enemies and obstacles

---
+++ Simpler Personal Project - Melanie Auman/Assets/Scripts/Movedown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movedown : MonoBehaviour
+{
+    public float speed = 25;
+
+    private float leftBound = -15;
+
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
